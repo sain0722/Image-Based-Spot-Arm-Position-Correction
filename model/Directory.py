ab4fe8e996c16cdb6c24574e3209e439fca68e50
smlee <sain0722@naver.com>
2023-06-27 11:40:17 +0900

    Remove folders including code from tracking.

--- model/Directory.py
+++
@@ -1,7 +0,0 @@
-class Directory:
-    def __init__(self, path):
-        self.path = path
-        self.subdirectories = []
-
-    def add_subdirectory(self, subdirectory):
-        self.subdirectories.append(subdirectory)
--- view/ui_to_py.bat
+++
@@ -1,4 +0,0 @@
-# set root=C:\Users\sangmin\anaconda3
-# call %root%\Scripts\activate.bat %root%
-call python -m PyQt5.uic.pyuic -x MainUI.ui -o Main_UI.py
-# pause
